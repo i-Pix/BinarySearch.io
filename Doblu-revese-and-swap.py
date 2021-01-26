0f53844933d743236002477bdfbd5b0dedd7c85c
Nitin <56484699+i-Pix@users.noreply.github.com>
2021-01-26 12:28:27 +0530

    Create Doblu-revese-and-swap.py

---
+++ Doblu-revese-and-swap.py
@@ -0,0 +1,22 @@
+class Solution:
+    def solve(self, n):
+        s = "xxy"
+        ct = 0
+        for i in range(1, n + 1):
+            if ct == 0:
+                s = s * 2
+            elif ct == 1:
+                s = s[::-1]
+            elif ct == 2:
+                temp = ""
+                for j in s:
+                    if j == "x":
+                        temp += "y"
+                    else:
+                        temp += "x"
+                s = temp
+            if ct == 2:
+                ct = 0
+            else:
+                ct += 1
+        return s
